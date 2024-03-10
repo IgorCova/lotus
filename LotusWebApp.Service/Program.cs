6219b5b99945ff857b6e2e5b3f8e99c32a6aa8e7
Igor Cova <emptyparam@gmail.com>
2024-03-10 20:59:00 +0300

    Add k8s and healthz

--- LotusWebApp.Service/Program.cs
+++ LotusWebApp.Service/Program.cs
@@ -1,11 +1,18 @@
+using LotusWebApp;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks()
+    .AddCheck<HealthCheck>("default");
 
 var app = builder.Build();
+
+app.MapHealthChecks("/healthz")
+    .RequireHost("*:8000");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
