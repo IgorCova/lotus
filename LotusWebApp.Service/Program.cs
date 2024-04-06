9d3dd03e3c1329dc45126d154abe6ec218a8182c
Igor Cova <emptyparam@gmail.com>
2024-04-06 22:50:40 +0300

    added metrics

--- LotusWebApp.Service/Program.cs
+++ LotusWebApp.Service/Program.cs
@@ -1,8 +1,11 @@
 using LotusWebApp;
 using LotusWebApp.Data;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.EntityFrameworkCore;
+using OpenTelemetry.Metrics;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<ContosoMetrics>();
 var configuration = builder.Configuration;
 
 builder.Services.RegisterContext(typeof(IContextRegistration), configuration);
@@ -18,6 +21,22 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddHealthChecks()
     .AddCheck<HealthCheck>("default");
 
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(meterProviderBuilder =>
+    {
+        meterProviderBuilder.AddPrometheusExporter();
+        meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Hosting",
+            "Microsoft.AspNetCore.Server.Kestrel");
+        meterProviderBuilder.AddView("http.server.request.duration",
+            new ExplicitBucketHistogramConfiguration
+            {
+                Boundaries =
+                [
+                    0, 0.005, 0.01, 0.025, 0.05,
+                    0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10
+                ]
+            });
+    });
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
@@ -28,12 +47,8 @@ using (var scope = app.Services.CreateScope())
 app.MapHealthChecks("/healthz")
     .RequireHost("*:8000");
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
@@ -45,4 +60,32 @@ app.MapGet("/health", () => new
     .WithOpenApi();
 
 app.MapControllers();
+app.MapPrometheusScrapingEndpoint();
+
+app.Use(async (context, next) =>
+{
+    var tagsFeature = context.Features.Get<IHttpMetricsTagsFeature>();
+    if (tagsFeature != null)
+    {
+        var source = context.Request.Query["utm_medium"].ToString() switch
+        {
+            "" => "none",
+            "social" => "social",
+            "email" => "email",
+            "organic" => "organic",
+            _ => "other"
+        };
+        tagsFeature.Tags.Add(new KeyValuePair<string, object?>("mkt_medium", source));
+    }
+
+    await next.Invoke();
+});
+
+app.MapGet("/", () => "Hello OpenTelemetry! ticks:"
+                      + DateTime.Now.Ticks.ToString()[^3..]);
+app.MapPost("/user", (UserDto model, ContosoMetrics metrics) =>
+{
+    metrics.UserCreated(model.Name);
+});
+
 app.Run();
